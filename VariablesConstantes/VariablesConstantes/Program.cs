1ca6e04660472eb827ac2735e839e60cc1b50218
SofiTD45 <sofia.trujillo@upb.edu.co>
2026-07-30 08:15:16 -0500

    Primer commit

---
+++ VariablesConstantes/VariablesConstantes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aquí va el código fuente
+            string nombre = "Sofia";
+            Console.WriteLine("Bienvedido al curso de fundamentos de programacion");
+        }
+    }
+}
