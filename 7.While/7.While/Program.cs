1f9a223f1daf3e4cfa51924f50b75514b54d35e9
SofiTD45 <sofia.trujillo@upb.edu.co>
2026-08-25 09:31:13 -0500

    while

---
+++ 7.While/7.While/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _7.While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. escribir un algoritmo que permita generar los primeros 5  enteros positivos y realizar y mostrar su suma.
+
+            /*int contador = 1;
+            int acumulador = 1;
+
+            while (contador < 5)
+            {
+                contador++;
+                acumulador = acumulador + contador;
+            }
+
+            Console.WriteLine($"la suma de los primero 5 números enteros positivos es: {acumulador}");*/
+
+            //crear un algoritmo que permita calcular la factorial de un número dado por el usuario. (5!= 1*2*3*4*5)
+
+            int num1 = 0;
+            int contador = 1;
+            int acomulador = 1;
+
+            Console.WriteLine("ingrese un numero entero positivo");
+            num1 = int.Parse(Console.ReadLine());
+
+            while
+            { }
+                
+
+
+        }
+    }
+}
