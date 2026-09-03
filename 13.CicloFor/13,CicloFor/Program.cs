c7b29f7a2b65d1f80da47a724cfbfc1c4253d0e2
SofiTD45 <sofia.trujillo@upb.edu.co>
2026-09-03 09:26:17 -0500

    Add files via upload

---
+++ 13.CicloFor/13,CicloFor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_CicloFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ciclo For
+
+            int acumulador = 0;
+
+            for ( int contador=1; contador<=5; contador++ )
+            {
+                acumulador+= contador;
+            }
+            Console.WriteLine( $"la suma de los primeros 5 numeros positivos enteros es: {acumulador}");
+        }
+    }
+}
